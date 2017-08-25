9b5933f6dccf0eb7347b26c65ad16df2c6e43048
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2017-08-25 06:18:18 -0400

    Added Bootstrap, enabled webpack configurations, a simple demo as a Vue.js component

--- app/index.js
+++ app/index.js
@@ -1,5 +1,13 @@
-require('./css/main.css')
+require('bootstrap/dist/css/bootstrap.min.css');
+require('./css/main.css');
+
 import Vue from 'vue';
+
+
+Vue.component('my-button', {
+	template: '<input type="button" class="btn btn-primary" value="Hello bootstrap">'
+})
+
 new Vue({
     el: '#app',
     data: {
